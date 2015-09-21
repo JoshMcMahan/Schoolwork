29c35dd98b7fb84c2dc4986185ac4f8ad3309da5
JoshMcMahan <Playme393>
2015-09-21 11:03:15 -0500

    Assingment 4A
    Solves a quadratic equation using input from the user

---
+++ CSCI 2473/Chapter 4/Assingment4A/ConsoleApplication1/Source.cpp
@@ -0,0 +1,43 @@
+//Josh McMahan
+//C Programming
+//Chapter 4 assignment 4A
+//Assignment4A.cpp
+//A program that takes input from the user and finds the 
+
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+//declares variables for A, B, C,
+double A;
+double B;
+double C;
+
+int main()
+{
+	
+	//Ask the user for input to be used in the quadratic equation
+	cout << "Enter a value for A: ";
+	cin >> A;
+	cout << "Enter a value for B: ";
+	cin >> B;
+	cout << "Enter a value for C: ";
+	cin >> C;
+
+	//declares variables for quad, quad2
+	double quad = (-B + sqrt(B * B - 4 * A * C)) / (2 * A);
+	double quad2 = (-B - sqrt(B * B - 4 * A * C)) / (2 * A);
+
+	//outputs the answer 
+	cout << "x1 = " << quad << endl;
+	cout << "x2 = " << quad2 << endl;
+
+
+
+	return 0;
+
+
+
+}

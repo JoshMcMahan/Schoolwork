e3c5cf44201fa7a9277f1b0332e2e5cb4e2164c5
JoshMcMahan <dmc2036@gmail.com>
2015-09-21 17:52:13 -0500

    Corrected
    Corrected an error

--- CSCI 2473/Chapter 4/Assingment4B/4B.cpp
+++ CSCI 2473/Chapter 4/Assingment4B/4B.cpp
@@ -18,33 +18,39 @@ int main()
 	ofstream Payoutfile;
 
 	//Assigning variables
-	char cat1, cat2, cat3;
+	char cat1;
+	char cat2;
+	char cat3;
 
-	double pay1 = 19312.34;
-	double pay2 = 26825.12;
-	double pay3 = 28562.40;
+	double pay1;
+	double pay2;
+	double pay3;
 
-	int num1 = 34;
-	int num2 = 56;
-	int num3 = 80;
+	int num1;
+	int num2;
+	int num3;
 
-	int avrgemp = (num1 + num2 + num3) / 3;
-	int avrgemp2 = (num1 + num2 + num3);
 
-	//Creating files for Payin and Payout
-	Payinfile.open("Payinfile.txt");
+	
+	Payinfile.open("input.txt");
+	//Creating files for Payout
 	Payoutfile.open("Payoutfile.txt");
 
-	Payinfile >> cat1, pay1, num1
-		>> cat2, pay2, num2
-		>> cat3, pay3, num3;
+	Payinfile >> cat1 >> pay1 >> num1
+		>> cat2 >> pay2 >> num2
+		>> cat3 >> pay3 >> num3;
+
+	int avgemp = (num1 + num2 + num3) / 3;
+	int totEmp = (num1 + num2 + num3);
+
+
 	//Calculating the average of each category
 	Payoutfile << "The average pay for category X is " << pay1 / num1 << endl
 		<< "The average pay for category Y is " << pay2 / num2 << endl
 		<< "The average pay for category Z is " << pay3 / num3 << endl
-		<< "The average number of employees is " << avrgemp << endl
-		<< "The average pay for all employees is " << (pay1 + pay2 + pay3) / avrgemp2;
-	
+		<< "The average number of employees is " << avgemp << endl
+		<< "The average pay for all employees is " << (pay1 + pay2 + pay3) / totEmp;
+
 	return 0;
 }
 

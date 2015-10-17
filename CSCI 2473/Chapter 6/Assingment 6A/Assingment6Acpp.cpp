a996a0c39dbb4860678e8621ac4bcd516e6c33f6
JoshMcMahan <Playme393>
2015-10-17 15:03:12 -0500

    Name place holder
    Created a folder for a future assingment.

---
+++ CSCI 2473/Chapter 6/Assingment 6A/Assingment6Acpp.cpp
@@ -0,0 +1,5 @@
+//Josh McMahan
+//C Programming
+//Chapter 6 assignment 6A
+//Assignment6A.cpp
+//A program that finds the median of three numbers inputed by the user
---
+++ CSCI 2473/Chapter 6/Assingment 6B/Assingment6B.cpp
@@ -0,0 +1,5 @@
+//Josh McMahan
+//C Programming
+//Chapter 6 assignment 6B
+//Assignment6B.cpp
+//A program that finds the median of three numbers inputed by the user

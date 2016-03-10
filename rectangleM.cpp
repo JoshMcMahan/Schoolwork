02deb16dd9fe88ee4be0a19e069b10ca2f753527
JoshMcMahan <dmc2036@gmail.com>
2016-03-09 21:19:03 -0600

    Create rectangleM.cpp

---
+++ rectangleM.cpp
@@ -0,0 +1,21 @@
+//Joshua McMahan
+//CSCI 2843
+//Assignment 2
+//Assignment2_main.cpp
+//To be added later
+
+#include <iostream>
+#include "rectangle.h"
+#include "rectangle.cpp"
+
+using namespace std;
+
+int main()
+{
+	double L;
+	double W;
+
+	rectangle obj1;
+	
+	
+}

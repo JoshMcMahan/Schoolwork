874bdb75ce147dd1fee0c974f0e4fdb75afb9172
JoshMcMahan <dmc2036@gmail.com>
2016-03-10 00:21:05 -0600

    Update rectangleM.cpp

--- rectangleM.cpp
+++ rectangleM.cpp
@@ -12,10 +12,24 @@ using namespace std;
 
 int main()
 {
-	double L;
-	double W;
+	double L = 0.0;
+	double W = 0.0;
 
 	rectangle obj1;
 	
-	
+	rectangle obj2;
+		obj2.setLength(7.0);
+		obj2.setWidth(3.2);
+	rectangle obj3;
+		obj3.setLength(6.3);
+	rectangle obj4;
+		obj4.setLength(25.0);
+		obj4.setWidth(30.0);
+	rectangle obj5 = obj2;
+
+	 obj2.getLength;
+	 cout << obj2.getLength;
+		
+
+		
 }

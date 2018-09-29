91ab81d17bfc9c7ba3b4bb31531ac9a9900f81b6
Josh McMahan <josh@joshmcmahan.com>
2018-09-29 00:15:58 -0500

    made timers and got them to display

--- Mobile App Development/Weekly Assignments/MW1_McMahan_Josh/MW1_McMahan_Joshua/ViewController.swift
+++ Mobile App Development/Weekly Assignments/MW1_McMahan_Josh/MW1_McMahan_Joshua/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -18,6 +19,9 @@ class ViewController: UIViewController {
     var userLap:Int = 0
     var nextUserLap:Int = 1
     var timer:Timer!
+    var time: Double = 0
+    var startTimer: Int = 0
+    var stopTimer: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,10 +37,12 @@ class ViewController: UIViewController {
             sender.setTitle("Stop", for: .normal)
             sender.setTitleColor(.red, for: .normal)
             updateLap()
+            handleTimerStart()
             break
         case "Stop":
             sender.setTitle("Start", for: .normal)
             sender.setTitleColor(.black, for: .normal)
+            
             break
         default:
             break
@@ -55,6 +61,24 @@ class ViewController: UIViewController {
             
         }
     }
+    
+    func handleTimerStart() {
+        startTimer = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: {_ in self.lapTime.text = self.createFormattedTimeString(time: self.startTimer)
+            self.totalTime.text = self.createFormattedTimeString(time: self.startTimer)
+            self.startTimer = self.startTimer + 1
+        })
+    }
+    
+    func createFormattedTimeString(time: Int) -> String {
+        
+        let hours = String(time / 36000)
+        let minutes = String(time % 36000 / 600)
+        let seconds = String(time % 36000 % 600 / 10)
+        let tenthofasecond = String(time % 10)
+        return "\(hours):\(minutes):\(seconds):\(tenthofasecond)"
+    }
+
         
     
 }

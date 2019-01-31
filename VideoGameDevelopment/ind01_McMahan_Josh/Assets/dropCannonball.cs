cea2a9b1a2dab75f37116f6bad13d3cba6f829dd
JoshMcMahan <github@joshmcmahan.com>
2019-01-31 03:09:04 -0600

    spawn cannonball
    Use Instantiate to spawn the cannonballs

---
+++ VideoGameDevelopment/ind01_McMahan_Josh/Assets/dropCannonball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropCannonball : MonoBehaviour
+{
+    public GameObject cannonball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)){
+            Instantiate(cannonball, transform);
+        }
+    }
+}
---
+++ VideoGameDevelopment/ind01_McMahan_Josh/Assets/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody2D characterBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       characterBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 movementVector = new Vector2(horizontal, vertical);
+
+        characterBody.AddForce(movementVector * speed);
+
+
+    }
+}

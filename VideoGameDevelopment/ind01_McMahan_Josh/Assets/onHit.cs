d26321cae4d0fb5d355b6f546e80a1ac639e7ed8
JoshMcMahan <github@joshmcmahan.com>
2019-02-06 21:36:01 -0600

    add score and timer
    Added the score counter and the timer

--- VideoGameDevelopment/ind01_McMahan_Josh/Assets/onHit.cs
+++ VideoGameDevelopment/ind01_McMahan_Josh/Assets/onHit.cs
@@ -1,32 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class onHit : MonoBehaviour
 {
     private AudioSource screamAudio;
     private int numberOfHits = 0;
-
     // Start is called before the first frame update
     void Start()
     {
         screamAudio = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // when collision is detected play audio 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
+       
         if (collision.gameObject.tag == "undertale" && numberOfHits <1)
         {
+            scoreScript.scoreValue += 1;
             screamAudio.Play();
 
         }
+
         numberOfHits++;
+
     }
 }
